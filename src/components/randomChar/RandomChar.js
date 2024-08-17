9abb7b0ef014da6c5297e64cb0b2ae0e7133df16
Michael <mishanhzer@gmail.com>
2024-08-17 14:35:15 +0500

    fixed spinner

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -59,7 +59,7 @@ class RandomChar extends Component {
         const {char, loading, error} = this.state;
 
         const errorMessage = error ? <ErrorMessage/> : null;
-        const spinner = loading ? <Spinner/> : null;
+        const spinner = (!error && loading) ? <Spinner/> : null;
         const content = !(loading || error) ? <View char={char}/> : null; 
         
         return (
